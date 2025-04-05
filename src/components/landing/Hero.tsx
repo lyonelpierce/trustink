8974081a3b8c33405e0d46c540a43e65da3771a9
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-04-05 11:12:17 -0400

    smart contracts

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { SignedOut } from "@clerk/nextjs";
 import WidthWrapper from "../WidthWrapper";
 import { buttonVariants } from "../ui/button";
-import { SignedOut } from "@clerk/nextjs";
 
 const Hero = () => {
   return (
     <WidthWrapper className="flex flex-col gap-4 items-center justify-center pt-20 h-screen">
       <h2 className="text-7xl font-semibold text-center">
-        The{" "}
+        A new generation of{" "}
         <span className="dark:bg-white text-white bg-black dark:text-black">
-          next generation
+          smart contracts.
         </span>{" "}
-        of contracts
       </h2>
       <p className="text-2xl text-muted-foreground">
         smart, dynamic, and personalized.
