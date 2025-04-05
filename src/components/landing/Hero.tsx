5607cc4a017a4fcff781bdac3f33ef78fab1f0c7
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-04-05 10:53:29 -0400

    hero component

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,14 +1,29 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 import WidthWrapper from "../WidthWrapper";
+import { buttonVariants } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div>
-      <WidthWrapper className="flex flex-col items-center justify-center pt-20 h-96">
-        <h2 className="text-6xl font-semibold">
-          A new era of smart agreements
-        </h2>
-      </WidthWrapper>
-    </div>
+    <WidthWrapper className="flex flex-col gap-4 items-center justify-center pt-20 h-screen">
+      <h2 className="text-7xl font-semibold text-center">
+        The{" "}
+        <span className="dark:bg-white text-white bg-black dark:text-black p-0.5">
+          next generation
+        </span>{" "}
+        of contracts
+      </h2>
+      <p className="text-2xl text-muted-foreground">
+        smart, dynamic, and personalized.
+      </p>
+
+      <Link
+        href="/sign-in"
+        className={cn(buttonVariants({ variant: "default" }))}
+      >
+        Get Started
+      </Link>
+    </WidthWrapper>
   );
 };
 
