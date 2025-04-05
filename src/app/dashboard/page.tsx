c27bf585272ad80c4a628671de8415ebd0723b99
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-04-05 12:56:19 -0400

    db types

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
