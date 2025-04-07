fb7e679488cad9a6252e7b2dba5cb9570ee34330
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-04-07 09:33:28 -0400

    no docs

--- src/components/dashboard/documents/Table/DocumentsTable.tsx
+++ src/components/dashboard/documents/Table/DocumentsTable.tsx
@@ -69,7 +69,7 @@ export function DocumentsTable<TData, TValue>({
           ) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
+                There are no documents yet. Add one to get started.
               </TableCell>
             </TableRow>
           )}
