88847ccae84994a0ca6377d1a964eb17c984380c
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-04-17 18:53:56 -0400

    remove unused code

--- src/components/viewer/SendModal.tsx
+++ src/components/viewer/SendModal.tsx
@@ -14,7 +14,6 @@ import {
   CredenzaClose,
   CredenzaFooter,
   CredenzaHeader,
-  CredenzaTrigger,
   CredenzaContent,
   CredenzaDescription,
 } from "../ui/credenza";
@@ -30,8 +29,9 @@ import { RadioGroup } from "../ui/radio-group";
 import { RadioGroupItem } from "../ui/radio-group";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LockIcon, LockOpenIcon, SendIcon, Loader2Icon } from "lucide-react";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useSession } from "@clerk/nextjs";
 const formSchema = z.object({
   title: z.string().min(1),
   access: z.enum(["public", "private"]),
@@ -45,7 +45,8 @@ const SendModal = ({
   documentId: string;
 }) => {
   const router = useRouter();
-
+  const { session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -55,6 +56,37 @@ const SendModal = ({
       access: "public",
     },
   });
+
+  const createClerkSupabaseClient = () => {
+    return createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        async accessToken() {
+          return session?.getToken() ?? null;
+        },
+      }
+    );
+  };
+
+  const supabase = createClerkSupabaseClient();
+
+  useEffect(() => {
+    const documentStatus = async () => {
+      const { data, error } = await supabase
+        .from("recipients")
+        .select("id, signer_id, user_id, status")
+        .eq("document_id", documentId);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(data);
+    };
+
+    documentStatus();
+  }, [session, documentId, supabase]);
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
@@ -80,119 +112,125 @@ const SendModal = ({
   };
 
   return (
-    <Credenza>
-      <CredenzaTrigger asChild>
-        <Button>
-          <SendIcon />
-          Review and Send
-        </Button>
-      </CredenzaTrigger>
-      <CredenzaContent>
-        <CredenzaHeader>
-          <CredenzaTitle>Review and Send</CredenzaTitle>
-          <CredenzaDescription>
-            Review the document and send it to the recipients.
-          </CredenzaDescription>
-        </CredenzaHeader>
-        <CredenzaBody>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-4"
-              id="send-document-form"
+    <>
+      <Button variant="outline" onClick={() => setIsOpen(true)}>
+        <SendIcon />
+        Review and Send
+      </Button>
+      <Credenza open={isOpen} onOpenChange={setIsOpen}>
+        <CredenzaContent>
+          <CredenzaHeader>
+            <CredenzaTitle>Review and Send</CredenzaTitle>
+            <CredenzaDescription>
+              Review the document and send it to the recipients.
+            </CredenzaDescription>
+          </CredenzaHeader>
+          <CredenzaBody>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+                id="send-document-form"
+              >
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Document Title</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="access"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Document Access</FormLabel>
+                      <FormControl>
+                        <RadioGroup
+                          defaultValue="card"
+                          className="grid grid-cols-2 gap-4"
+                          onValueChange={field.onChange}
+                          value={field.value}
+                        >
+                          <div>
+                            <RadioGroupItem
+                              value="public"
+                              id="public"
+                              className="peer sr-only"
+                              aria-label="Public"
+                            />
+                            <Label
+                              htmlFor="public"
+                              className="cursor-pointer flex gap-4 items-center rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                            >
+                              <LockOpenIcon className="flex-shrink-0" />
+                              <div className="flex flex-col gap-1">
+                                <span>Public</span>
+                                <span className="text-xs text-muted-foreground">
+                                  Document can be viewed by anyone with the
+                                  link.
+                                </span>
+                              </div>
+                            </Label>
+                          </div>
+                          <div>
+                            <RadioGroupItem
+                              value="private"
+                              id="private"
+                              className="peer sr-only"
+                              aria-label="Private"
+                            />
+                            <Label
+                              htmlFor="private"
+                              className="cursor-pointer flex gap-4 items-center rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                            >
+                              <LockIcon className="flex-shrink-0" />
+                              <div className="flex flex-col gap-1">
+                                <span>Private</span>
+                                <span className="text-xs text-muted-foreground">
+                                  Users will need to sign up to view the
+                                  document
+                                </span>
+                              </div>
+                            </Label>
+                          </div>
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </CredenzaBody>
+          <CredenzaFooter className="flex justify-end gap-2">
+            <Button
+              type="submit"
+              form="send-document-form"
+              disabled={isLoading}
             >
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Document Title</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="access"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Document Access</FormLabel>
-                    <FormControl>
-                      <RadioGroup
-                        defaultValue="card"
-                        className="grid grid-cols-2 gap-4"
-                        onValueChange={field.onChange}
-                        value={field.value}
-                      >
-                        <div>
-                          <RadioGroupItem
-                            value="public"
-                            id="public"
-                            className="peer sr-only"
-                            aria-label="Public"
-                          />
-                          <Label
-                            htmlFor="public"
-                            className="cursor-pointer flex gap-4 items-center rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                          >
-                            <LockOpenIcon className="flex-shrink-0" />
-                            <div className="flex flex-col gap-1">
-                              <span>Public</span>
-                              <span className="text-xs text-muted-foreground">
-                                Document can be viewed by anyone with the link.
-                              </span>
-                            </div>
-                          </Label>
-                        </div>
-                        <div>
-                          <RadioGroupItem
-                            value="private"
-                            id="private"
-                            className="peer sr-only"
-                            aria-label="Private"
-                          />
-                          <Label
-                            htmlFor="private"
-                            className="cursor-pointer flex gap-4 items-center rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                          >
-                            <LockIcon className="flex-shrink-0" />
-                            <div className="flex flex-col gap-1">
-                              <span>Private</span>
-                              <span className="text-xs text-muted-foreground">
-                                Users will need to sign up to view the document
-                              </span>
-                            </div>
-                          </Label>
-                        </div>
-                      </RadioGroup>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
-        </CredenzaBody>
-        <CredenzaFooter className="flex justify-end gap-2">
-          <Button type="submit" form="send-document-form" disabled={isLoading}>
-            {isLoading ? (
-              <Loader2Icon className="w-4 h-4 animate-spin" />
-            ) : (
-              <div className="flex items-center gap-2">
-                <SendIcon />
-                Send
-              </div>
-            )}
-          </Button>
-          <CredenzaClose asChild>
-            <Button variant="outline">Close</Button>
-          </CredenzaClose>
-        </CredenzaFooter>
-      </CredenzaContent>
-    </Credenza>
+              {isLoading ? (
+                <Loader2Icon className="w-4 h-4 animate-spin" />
+              ) : (
+                <div className="flex items-center gap-2">
+                  <SendIcon />
+                  Send
+                </div>
+              )}
+            </Button>
+            <CredenzaClose asChild>
+              <Button variant="outline">Close</Button>
+            </CredenzaClose>
+          </CredenzaFooter>
+        </CredenzaContent>
+      </Credenza>
+    </>
   );
 };
 
