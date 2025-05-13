25389f6a3c976ab9f37a129d6ab57ac8368ff52f
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-05-13 14:01:55 -0400

    hero seciton

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -22,12 +22,12 @@ export default function Home() {
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
         <Hero />
       </div>
-      <ProblemSection />
-      <DemoSection />
-      <TechStackSection />
-      <TractionSection />
-      <TeamSection />
-      <CTASection />
+      {/* <ProblemSection /> */}
+      {/* <DemoSection /> */}
+      {/* <TechStackSection /> */}
+      {/* <TractionSection /> */}
+      {/* <TeamSection /> */}
+      {/* <CTASection /> */}
     </div>
   );
 }
