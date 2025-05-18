192bfbd69c9cb1738def0b58882e2b25aae6befa
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-05-18 10:29:40 -0400

    provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider";
-import { DemoModeProvider } from "@/contexts/DemoModeContext";
+import { ConvexReactClient } from "convex/react";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { DemoModeProvider } from "@/contexts/DemoModeContext";
 
 export const metadata: Metadata = {
   title: {
@@ -14,6 +16,10 @@ export const metadata: Metadata = {
   description: "First Smart Agreements Platform",
 };
 
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string
+);
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,21 +27,23 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className="font-sans antialiased">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <DemoModeProvider>
-              <ErrorBoundary>{children}</ErrorBoundary>
-              <Toaster position="bottom-right" richColors />
-            </DemoModeProvider>
-          </ThemeProvider>
-        </body>
-      </html>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <html lang="en" suppressHydrationWarning>
+          <body className="font-sans antialiased">
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <DemoModeProvider>
+                <ErrorBoundary>{children}</ErrorBoundary>
+                <Toaster position="bottom-right" richColors />
+              </DemoModeProvider>
+            </ThemeProvider>
+          </body>
+        </html>
+      </ConvexProviderWithClerk>
     </ClerkProvider>
   );
 }
